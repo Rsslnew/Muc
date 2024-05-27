0d97fd41bd3f2eee6d88894870956c8e337bffcd
ElizabethXty <elizabethhh198@gmail.com>
2024-05-27 15:26:05 +0700

    Update config.py

--- config/config.py
+++ config/config.py
@@ -78,7 +78,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/ElizabethXty/MusicIndo",
+    "https://github.com/ElizabethXty/MusikIndo",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 

69a9df58002a748dfbb040cd354534bd8a829233
ElizabethXty <elizabethhh198@gmail.com>
2024-05-28 07:20:13 +0700

    Freze pytgcalls 2.0.1 ke 1.2.9

--- YukkiMusic/utils/decorators/mustjoin.py
+++ YukkiMusic/utils/decorators/mustjoin.py
@@ -1,40 +1,38 @@
-from pyrogram.errors import ChatAdminRequired, ChatWriteForbidden, UserNotParticipant
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
 from YukkiMusic import app
-from config import MUST_JOIN
 
+#--------------------------
 
-def subcribe(func):
-    async def wrapper(_, message: Message):
-        user_id = message.from_user.id
-        user_name = message.from_user.first_name
-        rpk = "[" + user_name + "](tg://user?id=" + str(user_id) + ")"
-        if not MUST_JOIN:  # Not compulsory
-            return
+MUST_JOIN = "miniiiiupdate"
+#------------------------
+@app.on_message(filters.incoming & filters.private, group=-1)
+async def must_join_channel(app: Client, msg: Message):
+    if not MUST_JOIN:
+        return
+    try:
         try:
+            await app.get_chat_member(MUST_JOIN, msg.from_user.id)
+        except UserNotParticipant:
+            if MUST_JOIN.isalpha():
+                link = "https://t.me/" + MUST_JOIN
+            else:
+                chat_info = await app.get_chat(MUST_JOIN)
+                link = chat_info.invite_link
             try:
-                await app.get_chat_member(MUST_JOIN, message.from_user.id)
-            except UserNotParticipant:
-                if MUST_JOIN.isalpha():
-                    yuhu = "https://t.me/" + MUST_JOIN
-                else:
-                    chat_info = await app.get_chat(MUST_JOIN)
-                    chat_info.invite_link
-                try:
-                    await message.reply(
+                await message.reply(
                         f"ʜᴀʟʟᴏ ᴋᴀᴋ😊 . ᴀɢᴀʀ ʙɪsᴀ ᴍᴇɴɢɢᴜɴᴀᴋᴀɴ ʙᴏᴛ ᴀɴᴅᴀ ʜᴀʀᴜs ᴍᴀsᴜᴋ ᴋᴇ ᴄʜᴀɴɴᴇʟ ᴛᴇʀʟᴇʙɪʜ ᴅᴀʜᴜʟᴜ!. sɪʟᴀʜᴋᴀɴ ᴋʟɪᴋ ᴛᴏᴍʙᴏʟ ᴅɪ ʙᴀᴡᴀʜ ᴜɴᴛᴜᴋ ᴊᴏɪɴ ᴋᴇ ᴄʜᴀɴɴᴇʟ, sᴇᴛᴇʟᴀʜ ɪᴛᴜ sɪʟᴀʜᴋᴀɴ ᴘʟᴀʏ ᴍᴜsɪᴋ/ᴠɪᴅᴇᴏ ᴋᴀᴍᴜ",
-                        disable_web_page_preview=True,
-                        reply_markup=InlineKeyboardMarkup(
-                            [[InlineKeyboardButton("👻 Jᴏɪɴ ɢʀᴏᴜᴘ ", url=yuhu)]]
-                        ),
+                    reply_markup=InlineKeyboardMarkup(
+                        [
+                            [
+                                InlineKeyboardButton("Jᴏɪɴ sɪɴɪ", url=link),
+                            ]
+                        ]
                     )
-                    await message.stop_propagation()
-                except ChatWriteForbidden:
-                    pass
-        except ChatAdminRequired:
-            await message.reply(
-                f"Saya bukan admin di chat MUST_JOIN chat : {MUST_JOIN} !"
-            )
-        return await func(_, message)
-
-    return wrapper
+                )
+                await msg.stop_propagation()
+            except ChatWriteForbidden:
+                pass
+    except ChatAdminRequired:
+        print(f"๏ᴘʀᴏᴍᴏᴛᴇ ᴍᴇ ᴀs ᴀɴ ᴀᴅᴍɪɴ ɪɴ ᴛʜᴇ ᴍᴜsᴛ_Jᴏɪɴ ᴄʜᴀᴛ ๏: {MUST_JOIN} !")

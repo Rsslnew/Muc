992a2898f9b8962df73cb8db5a3dfdd786f60819
ElizabethXty <elizabethhh198@gmail.com>
2024-05-30 01:11:21 +0700

    Update Afk.py

--- YukkiMusic/plugins/tools/Afk.py
+++ YukkiMusic/plugins/tools/Afk.py
@@ -3,9 +3,9 @@ from config import BOT_USERNAME
 from pyrogram.enums import MessageEntityType
 from pyrogram import filters
 from pyrogram.types import Message
-from AarohiX import app
-from AarohiX.utils.decorators.readable_time import get_readable_time
-from AarohiX.utils.Databases.afkdb import add_afk, is_afk, remove_afk
+from YukkiMusic import app
+from YukkiMusic.utils.decorators.readable_time import get_readable_time
+from YukkiMusic.utils.Databases.afkdb import add_afk, is_afk, remove_afk
 
 
 

c6baa5ed45886330ee5a128afb8b7fec60b9c884
ElizabethXty <elizabethhh198@gmail.com>
2024-06-02 16:44:44 +0700

    Update play.py

--- YukkiMusic/utils/decorators/play.py
+++ YukkiMusic/utils/decorators/play.py
@@ -63,7 +63,7 @@ def PlayWrapper(command):
                 return await app.leave_chat(message.chat.id)
 
         if MUST_JOIN:
-            return await subcribe(client, message):
+            return await subcribe(client.message)
         if await is_commanddelete_on(message.chat.id):
             try:
                 await message.delete()

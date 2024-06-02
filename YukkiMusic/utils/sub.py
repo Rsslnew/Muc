02c682666b5bcc7dc58706a917b98e0e26b40f18
ElizabethXty <elizabethhh198@gmail.com>
2024-06-02 15:09:30 +0700

    Fix missing

--- YukkiMusic/utils/sub.py
+++ YukkiMusic/utils/sub.py
@@ -13,7 +13,7 @@ async def subcribe(client, message):
         await app.get_chat_member(config.JOIN_HELA, message.from_user.id)
     except UserNotParticipant:
         try:
-            link = f"t.me/{config.JOIN_DULU}"
+            link = f"t.me/{config.JOIN_HELA}"
             await message.reply(
                 f"**ʜᴀʟʟᴏ ᴋᴀᴋ😊 {message.from_user.mention} ᴀɢᴀʀ ʙɪsᴀ ᴍᴇɴɢɢᴜɴᴀᴋᴀɴ ʙᴏᴛ ᴀɴᴅᴀ ʜᴀʀᴜs ᴍᴀsᴜᴋ ᴋᴇ ɢʀᴏᴜᴘ ᴛᴇʀʟᴇʙɪʜ ᴅᴀʜᴜʟᴜ!. sɪʟᴀʜᴋᴀɴ ᴋʟɪᴋ ᴛᴏᴍʙᴏʟ ᴅɪ ʙᴀᴡᴀʜ ᴜɴᴛᴜᴋ ᴊᴏɪɴ ᴋᴇ ɢʀᴏᴜᴘ, sᴇᴛᴇʟᴀʜ ɪᴛᴜ sɪʟᴀʜᴋᴀɴ ᴘʟᴀʏ ᴍᴜsɪᴋ/ᴠɪᴅᴇᴏ ᴋᴀᴍᴜ**",
             reply_markup=InlineKeyboardMarkup(
